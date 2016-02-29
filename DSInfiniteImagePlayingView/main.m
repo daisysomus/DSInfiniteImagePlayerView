8b0ac9c8e49f9cc63dc391ada6000107b1092e90
daisysomus <daisysomus@gmail.com>
2016-02-29 14:53:03 +0800

    Initial Commit

---
+++ DSInfiniteImagePlayingView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DSInfiniteImagePlayingView
+//
+//  Created by liaojinhua on 16/2/29.
+//  Copyright © 2016年 Liao jinhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
